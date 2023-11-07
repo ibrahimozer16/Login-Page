bf6b6c62bac2919d967155e76cb25b4ec74c93eb
unknown <ibrahimozer161616@gmail.com>
2023-11-07 18:28:42 +0300

    login page

---
+++ firebase.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBaKwf4MfpHTVLdSV_OHDyPeDgX7Bdo7fI",
+  authDomain: "login-da14b.firebaseapp.com",
+  projectId: "login-da14b",
+  storageBucket: "login-da14b.appspot.com",
+  messagingSenderId: "1035688869472",
+  appId: "1:1035688869472:web:629903de34ae1849fdcd05"
+};
+
+// Initialize Firebase
+if(!firebase.apps.length)
+{
+firebase.initializeApp(firebaseConfig);
+}
+const auth = firebase.auth();
+
+export {auth};
